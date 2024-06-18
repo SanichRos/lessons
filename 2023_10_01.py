d47403a71f553e8a3cb96fb18ea25375c48cc880
SanichRos <megapayalnik@mail.ru>
2024-06-19 02:03:37 +0300

    2

--- 2023_10_01.py
+++ 2023_10_01.py
@@ -3,9 +3,11 @@ def get_matrix(n, m, value):
     for i in range(n):
         matrix.append(i)
         for j in range(m):
-            matrix[i].append(j)
+            #принт для дебага
+            print(i , j, matrix[i])
+            matrix[i] = (j)
 
     return matrix
 
 result1 = get_matrix(3, 5, 10)
-print(result1)
+# print(result1)
