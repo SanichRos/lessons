72972426792b78cba404e948a407cc9103912344
SanichRos <megapayalnik@mail.ru>
2024-08-15 01:44:27 +0300

    1

---
+++ 2023_10_07.py
@@ -0,0 +1,13 @@
+def print_params(a = 1, b = 'строка', c = True):
+    print(a,b,c)
+print_params()
+print_params(b = 25)
+print_params(c = [1,2,3])
+
+values_list = [5,'text',True]
+values_dict = {'a':6, 'b':'image', 'c':False}
+print_params(*values_list)
+print_params(**values_dict)
+
+values_list_2 = [54.32, 'Строка' ]
+print_params(*values_list_2, 42)
